cb26a3b79846bfd08799c240ba40391bf15c0d82
Mohammad Nasr <nasrmohammad661@gmail.com>
2026-03-23 12:55:54 -0700

    added the full package which includes the cylinder pipeline node

---
+++ src/active_perception/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+package_name = 'active_perception'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name],
+        ),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mohammadnsr1',
+    maintainer_email='nasrmohammad661@gmail.com',
+    description=(
+        'ROS 2 active perception nodes for cylinder detection and target '
+        'estimation from point clouds.'
+    ),
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'cylinder_finder = active_perception.cylinder_finder:main',
+        ],
+    },
+)
